31251d4ea3fc523ec013fcccd99ef421a5b1eb7e
n19awaf <nawaf13-@hotmail.com>
2024-02-15 16:15:21 +0800

    first commit

---
+++ 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Book.java
@@ -0,0 +1,40 @@
+package com.luv2code.springbootlibrary.entity;
+
+
+import lombok.Data;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private  long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private String copies;
+
+    @Column(name = "copies_available" )
+    private String copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+
+}
