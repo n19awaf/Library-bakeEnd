e823ecc200ce5df03a892e7739512585b480137f
N19awaf <nawaf13-@hotmail.com>
2024-02-29 17:56:39 +0800

    last updated

--- 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/utils/ExtractJWT.java
+++ 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/utils/ExtractJWT.java
@@ -1,5 +1,9 @@
 package com.luv2code.springbootlibrary.utils;
 
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ExtractJWT {
 
 
@@ -8,5 +12,22 @@ public class ExtractJWT {
         token.replace("Bearer","");
 
         String[] chunks = token.split("\\.");
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+
+        String payload = new String(decoder.decode(chunks[1]));
+
+        String[] entries = payload.split(",");
+        Map<String, String> map = new HashMap<String, String>();
+
+        for (String entry: entries){
+            String[] keyValue = entry.split(":");
+            if (keyValue[0].equals("\"sub\""));
+
+            int remove = 1;
+            if (keyValue[1].endsWith("}")){
+                remove = 2;
+            }
+            keyValue[1] = keyValue[1].substring(0,keyValue[1].length() - remove);
+        }
     }
 }
