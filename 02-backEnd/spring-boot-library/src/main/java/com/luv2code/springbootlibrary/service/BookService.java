577532a1049310d9c0de99968585d0569f9eb432
N19awaf <nawaf13-@hotmail.com>
2024-03-11 01:47:37 +0800

    last updated

--- 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
+++ 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
@@ -108,4 +108,20 @@ public class BookService {
 
     }
 
+    //return Book
+    public void returnBook (String userEmail, Long bookId) throws Exception {
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if (!book.isPresent() || validateCheckout == null) {
+            throw new Exception("Book does not exist or not checked out by user");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+
+        bookRepository.save(book.get());
+        checkoutRepository.deleteById(validateCheckout.getId());
+    }
+
 }
