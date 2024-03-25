5d793996dded75007a7093c20e2ad054cd26246a
N19awaf <nawaf13-@hotmail.com>
2024-03-26 03:58:27 +0800

    last updated

---
+++ 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/requestmodels/AddBookRequest.java
@@ -0,0 +1,18 @@
+package com.luv2code.springbootlibrary.requestmodels;
+
+public class AddBookRequest {
+
+    private String title;
+
+    private String author;
+
+    private String description;
+
+    private int copies;
+
+    private String category;
+
+    private String img;
+
+
+}
---
+++ 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/AdminService.java
@@ -0,0 +1,4 @@
+package com.luv2code.springbootlibrary.service;
+
+public class AdminService {
+}
