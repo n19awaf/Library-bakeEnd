549fe4bdfabf4d83ce5fcbfbc1fb16199c70a344
N19awaf <nawaf13-@hotmail.com>
2024-03-30 11:04:31 +0800

    last updated

--- 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/PaymentController.java
+++ 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/PaymentController.java
@@ -1,11 +1,15 @@
 package com.luv2code.springbootlibrary.controller;
 
 
+import com.luv2code.springbootlibrary.requestmodels.PaymentInfoRequest;
 import com.luv2code.springbootlibrary.service.PaymentService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("https://localhost:3000")
 @RestController
@@ -18,4 +22,23 @@ public class PaymentController {
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoRequest paymentInfoRequest) throws StripeException {
+
+        PaymentIntent paymentIntent = paymentService.createPaymentIntent(paymentInfoRequest);
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+    }
+
+    @PutMapping("/payment-complate")
+    public ResponseEntity<String> stripePaymentComplete(@RequestHeader(value = "Authorization") String token) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (userEmail == null){
+            throw new Exception("User email is missing");
+        }
+        return paymentService.stripePayment(userEmail);
+    }
+
 }
