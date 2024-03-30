b7cd8f901d3159d634ea139fa02c4187e09fcf83
N19awaf <nawaf13-@hotmail.com>
2024-03-30 09:57:21 +0800

    last updated

--- 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/PaymentService.java
+++ 02-backEnd/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/PaymentService.java
@@ -1,11 +1,16 @@
 package com.luv2code.springbootlibrary.service;
 
 import com.luv2code.springbootlibrary.dao.PaymentRepository;
+import com.luv2code.springbootlibrary.requestmodels.PaymentInfoRequest;
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
 
 @Service
 @Transactional
@@ -17,5 +22,19 @@ public class PaymentService {
     public PaymentService(PaymentRepository paymentRepository, @Value("${stripe.key.secret}") String secretKey) {
         this.paymentRepository = paymentRepository;
         Stripe.apiKey = secretKey;
+    }
+
+
+    public PaymentIntent createPaymentIntent(PaymentInfoRequest paymentInfoRequest) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfoRequest.getAmount());
+        params.put("current", paymentInfoRequest.getCurrency());
+        params.put("payment_method_type", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
+
     }
 }
